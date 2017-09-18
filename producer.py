26c415f5ebc1f745aa25d47c5f82997150c5c737
Biru Cui <biru@exablox.com>
2017-09-18 10:49:11 +0800

    kafkaproducer.py: test producer

---
+++ producer.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+#
+# https://docs.confluent.io/current/clients/confluent-kafka-python/
+#
+
+import os
+import tempfile
+
+from confluent_kafka import Producer
+
+def get_keytab(url):
+    try:
+        # python 3
+        from urllib.request import urlopen
+    except ImportError:
+        # python 2
+        from urllib2 import urlopen
+
+    fd, keytab_file = tempfile.mkstemp()
+    r = urlopen(url)
+    with os.fdopen(fd, "w+b") as f:
+        while True:
+            chunk = r.read(4096)
+            if not chunk:
+                break
+            f.write(chunk)
+    return keytab_file
+
+def producer(data):
+    env_list = [
+        'kafka_principal',
+        'kafka_keytab',
+        'kafka_brokers',
+        'kafka_topic',
+    ]
+    for env in env_list:
+        envv = os.environ.get(env)
+        if envv:
+            globals()[env] = envv
+        else:
+            raise Exception("missing env '{}'".format(env))
+
+    p = Producer({
+        'security.protocol': 'sasl_plaintext',
+        'sasl.kerberos.principal': kafka_principal,
+        'sasl.kerberos.keytab': get_keytab(kafka_keytab),
+        'bootstrap.servers': kafka_brokers,
+    })
+
+    for d in data:
+        p.produce(kafka_topic, d.encode('utf-8'))
+
+    p.flush()
+
+f __name__ == "__main__":
+    data = ['1', '2', '3', '4', '5']
+    producer(data)
